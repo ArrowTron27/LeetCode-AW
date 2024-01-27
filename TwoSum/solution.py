7ac5bc5ceda041decf8529f927bd6bb36b9e5c66
Aaron Wong <aaronwong2702@gmail.com>
2024-01-26 17:57:32 -0800

    added comments

--- TwoSum/solution.py
+++ TwoSum/solution.py
@@ -5,13 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        
+        # This is the return value array
         returnVal = []
+        
+        #number to add up the 2 sums
         num = 0
         
+        
+        # Nested for loop to search arrays
         for i in range(len(nums)):
             for j in range(len(nums)):
+                # Get the num of the array indexes
                 num = nums[i] + nums[j]
-                if (num == target and i!=j):
+                if (num == target and i!=j): # If num = target, and i and j are not the same index
+                    # Add to the return value array
                     returnVal.append(i)
                     returnVal.append(j)
                     return returnVal
